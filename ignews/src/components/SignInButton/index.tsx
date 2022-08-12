f4cf4c96f1c1fe415f1619a790cb4dd4b981dff1
Vinicius Benso <vinibenso@gmail.com>
2022-08-12 16:33:36 -0300

    login validation

--- ignews/src/components/SignInButton/index.tsx
+++ ignews/src/components/SignInButton/index.tsx
@@ -1,26 +1,30 @@
 import styles from './styles.module.scss'
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
+import { signIn, useSession, signOut } from 'next-auth/react'
 
 export function SignInButton() {
 
-  const isUserLoggedIn = true;
+  const { data: session } = useSession()
 
-  return  isUserLoggedIn ? ( <button 
-    type='button'
-    className={styles.signInButton}
-    >
-      <FaGithub color="#04d361" />
-      Vinicius Benso
-      <FiX color="#737380" className={styles.closeIcon} />
-    </button>
-    ) : ( 
-      <button 
+  return session ? (
+    <button
       type='button'
       className={styles.signInButton}
-      >
-        <FaGithub color="#EBA417" />
-        Sign in with Github
-      </button>
-    );
+      onClick={()=> signOut()}
+    >
+      <FaGithub color="#04d361" />
+      {session.user.name}
+      <FiX color="#737380" className={styles.closeIcon} />
+    </button>
+  ) : (
+    <button
+      type='button'
+      className={styles.signInButton}
+      onClick={() => signIn('github')}
+    >
+      <FaGithub color="#EBA417" />
+      Sign in with Github
+    </button>
+  );
 }
