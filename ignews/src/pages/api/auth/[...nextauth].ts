dac8f51f96857d700d22e592b7d0660f6c45ce05
vinibenso <vinibenso@gmail.com>
2022-10-11 19:54:23 -0300

    next auth

--- ignews/src/pages/api/auth/[...nextauth].ts
+++ ignews/src/pages/api/auth/[...nextauth].ts
@@ -1,16 +1,16 @@
 import { query  as q} from "faunadb"
 import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import GitHubProvider from "next-auth/providers/github"
 
 import { fauna } from '../../../services/fauna'
 
 export default NextAuth({
  
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      authorization: {params: {scope: 'read:user'}}
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+      authorization: {params: {scope: 'read:user user:email'}}
     }),
   ],
 
